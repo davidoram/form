91d6a44787f6becf3dfc64ea253cbdfa6e3e590d
David Oram <david.oram99@gmail.com>
2018-09-03 06:39:44 +1200

    Added loglevel + sslmodel args. Include the db in context

--- cmd/f-site/main.go
+++ cmd/f-site/main.go
@@ -7,10 +7,10 @@ import (
 
 	"github.com/davidoram/form/lib/formdb"
 
+	flags "github.com/jessevdk/go-flags"
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/middleware"
-
-	flags "github.com/jessevdk/go-flags"
+	lslog "github.com/labstack/gommon/log"
 )
 
 // CmdOptions defines the command line options
@@ -20,9 +20,11 @@ type CmdOptions struct {
 	DbName    string `long:"db-name" description:"Database name" required:"true" env:"F_DB_NAME"`
 	DbUser    string `long:"db-user" description:"Database username" required:"true" env:"F_DB_USER"`
 	DbPass    string `long:"db-password" description:"Database password" required:"true" env:"F_DB_PASSWORD"`
-	DBSSLMode string `long:"db-ssl-mode" description:"Database sslmode" required:"false" env:"F_DB_SSLMODE"`
+	DBSSLMode string `long:"db-ssl-mode" description:"Database sslmode" default:"prefer" choice:"disable" choice:"allow" choice:"prefer" choice:"require" choice:"verify-ca" choice:"verify-full" required:"false" env:"F_DB_SSLMODE"`
 
 	HttpPort int `long:"http-port" description:"HTTP port to run on" required:"true" env:"F_HTTP_PORT"`
+
+	LogLevel int `long:"log-level" descripton:"Logging level, 1=DEBUG, 2=INFO, 3=WARN, 4=ERROR" default:"2" choice:"1" choice:"2" choice:"3" choice:"4"  required:"false" env:"F_LOGLEVEL"`
 }
 
 func main() {
@@ -34,14 +36,16 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
+	e.Logger.SetLevel(lslog.Lvl(opts.LogLevel))
+	e.Logger.Info("f-server booting")
 
-	e.Logger.Info("Opening db connection...")
+	e.Logger.Info("Opening db connection")
 	db, err := formdb.Connect(opts.DbHost, opts.DbUser, opts.DbName, opts.DbPass, opts.DBSSLMode, opts.DbPort)
 	if err != nil {
 		panic(err)
 	}
 
-	e.Logger.Info("Migrating the database...")
+	e.Logger.Info("Migrating the database")
 	n, err := formdb.Migrate(db)
 	if err != nil {
 		log.Fatal("db migrations failed: ", err)
@@ -49,6 +53,12 @@ func main() {
 	e.Logger.Infof("%d migrations applied", n)
 
 	// Middleware
+	e.Use(func(h echo.HandlerFunc) echo.HandlerFunc {
+		return func(c echo.Context) error {
+			cc := &FormContext{c, db}
+			return h(cc)
+		}
+	})
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
 
@@ -61,5 +71,7 @@ func main() {
 
 // Handler
 func hello(c echo.Context) error {
-	return c.String(http.StatusOK, "Hello, World!")
+	fc := c.(*FormContext)
+	_, err := fc.DB.Exec("select 1")
+	return c.String(http.StatusOK, fmt.Sprintf("Hello, World! %v", err))
 }
